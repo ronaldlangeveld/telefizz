f1765013563d3f67370ca11b8bacc7e8f3df745d
Ronald Langeveld <hi@ronaldlangeveld.com>
2026-01-06 18:23:50 +0900

    Remove nested tele_fizz folders causing Zeitwerk error

--- test/controllers/telefizz/tele_fizz/board_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Telefizz::BoardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get telefizz_board_index_url
-    assert_response :success
-  end
-end
